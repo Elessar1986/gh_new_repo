80bbf4421c6b7ccfaf24bf9cde84e9a1fa290993
Арепьев Иван Юриевич <ivan.arepjev@gmail.com>
2017-08-18 17:51:04 +0300

    add notify

--- TestGitHub/TestGitHub/newTestClass.cs
+++ TestGitHub/TestGitHub/newTestClass.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,12 @@ namespace TestGitHub
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+
+        public void Notify ([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
 
     }
 }
